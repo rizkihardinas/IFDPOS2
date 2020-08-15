c08a64a0c6b1771c195a4c46e3af19bc0899bb2d
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2020-08-15 17:39:34 +0700

    Update Advance.sql

--- Advance.sql
+++ Advance.sql
@@ -6,3 +6,20 @@ create view view_grouping_products as
 	From
 		divisionproduct division RIGHT JOIN categoryproduct category ON division.id = category.division_product_id
 		RIGHT JOIN subcategoryproduct subcategory ON category.id = subcategory.category_product_id
+
+CREATE VIEW v_products as
+	SELECT 
+	view_grouping_products.*,
+	products.*,
+	brand.name as brand,
+	suppliers.name as supplier,
+	unit.name as unit,
+	producttype.name as type
+	FROM view_grouping_products 
+	JOIN products ON view_grouping_products.subcategory_id = products.sub_category_id
+	JOIN brand ON brand.id = products.brand_id
+	JOIN suppliers ON suppliers.id = products.suppliers_id
+	JOIN unit ON unit.id = products.unit_id
+	JOIN producttype ON producttype.id = products.type_product_id
+
+CREATE VIEW  view_price_detail aS SELECT price.name,IFNULL(pricedetail.price,0) as price,IFNULL(pricedetail.products_id,0) as products_id FROM `price` LEFT JOIN pricedetail ON price.id = pricedetail.price_id
